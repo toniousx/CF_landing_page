66c246ffeac2617b2c80cd7571842a437cc93d41
Tonio Serna <antonelo@gmail.com>
2018-05-09 11:56:12 +0200

    Make click_on test green

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'courses/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'courses#index'
 end
--- spec/features/get_course_information_spec.rb
+++ spec/features/get_course_information_spec.rb
@@ -4,7 +4,7 @@ feature 'get course information' do
   scenario 'become ui designer' do
     visit '/'
     expect(page).to have_content('Become a UI Designer')
-    # click_on('Become a UI Designer')
+    click_on('Become a UI Designer')
     # expect(page).to have_content('Monday, May 14th, 2018')
     # expect(page).to have_content('$4999')
   end
