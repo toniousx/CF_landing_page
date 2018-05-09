f8a1effb235c096506a6a57379e4b8d1da0b4dfa
Tonio Serna <antonelo@gmail.com>
2018-05-09 10:43:08 +0200

    Add scenario & first BDD-TDD red/failing test

---
+++ spec/features/get_course_information_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'get course information' do
+  scenario 'become ui designer' do
+    visit '/'
+    # expect(page).to have_content('Become a UI Designer')
+    # click_on('Become a UI Designer')
+    # expect(page).to have_content('Monday, May 14th, 2018')
+    # expect(page).to have_content('$4999')
+  end
+end
