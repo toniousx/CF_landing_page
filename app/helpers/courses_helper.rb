d9a5f06e3bf84ccdbf22531378f2636be535bfcf
Tonio Serna <antonelo@gmail.com>
2018-05-09 13:04:48 +0200

    Add route for :slug courses#show, helper methods, :show link  & make all tests green

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,9 @@
 module CoursesHelper
+  def next_start_date
+    @course.start_dates.first
+  end
+
+  def current_course_title
+    @courses.each { |course| return course.title if course.slug == @course.slug }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'courses#index'
+
+  get ':slug', to: 'courses#show' # *Needs to be the last to don't overwrite any future roots
 end
