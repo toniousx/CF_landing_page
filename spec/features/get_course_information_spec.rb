d833d3fbd3b5938cd8c5d0f06cff4d0d2fe58e87
Tonio Serna <antonelo@gmail.com>
2018-05-09 13:01:00 +0200

    Uncomment pending feature tests

--- spec/features/get_course_information_spec.rb
+++ spec/features/get_course_information_spec.rb
@@ -5,7 +5,7 @@ feature 'get course information' do
     visit '/'
     expect(page).to have_content('Become a UI Designer')
     click_on('Become a UI Designer')
-    # expect(page).to have_content('Monday, May 14th, 2018')
-    # expect(page).to have_content('$4999')
+    expect(page).to have_content('Monday, May 14th, 2018')
+    expect(page).to have_content('$4999')
   end
 end
