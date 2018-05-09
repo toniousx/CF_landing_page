b9c4c38da8e65f461a036a824156dc40e41e88f2
Tonio Serna <antonelo@gmail.com>
2018-05-09 12:59:26 +0200

    Refactor courses_controller & add :show

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,16 @@
 class CoursesController < ApplicationController
-  def index
+  before_action :set_courses
+
+  def index; end
+
+  def show
+    @course = Course.import_course(params[:slug])
+    @total_price = Course.import_course_price(params[:slug])
+  end
+
+  private
+
+  def set_courses
     @courses = Course.import_courses
   end
 end
