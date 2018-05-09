0734cbccc853e9d4d8cb617338426ef47275aeb9
Tonio Serna <antonelo@gmail.com>
2018-05-09 12:57:57 +0200

    Hotfix: redo course changes

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
+require 'open-uri' # we need to requiree it in order to avoid conflicts with Kernel#open  *see line 17
+
 class Course
   include ActiveModel::Model
 
@@ -20,7 +22,7 @@ class Course
   private_class_method def self.json_rest_courses(course = nil)
     url = "https://careerfoundry.com/en/api/courses/#{course}"
     Rails.cache.fetch(url, expires_in: 1.hour) do
-      JSON.parse(OpenURI::OpenRead.open(url).read)
+      JSON.parse(open(url).read) # rubocop:disable Security/Open
     end
   end
 end
