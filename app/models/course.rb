411d14f87d44afccdf60bd1ff04f70a46f7d42be
Tonio Serna <antonelo@gmail.com>
2018-05-09 11:43:26 +0200

    Add ActiveModel::Model , .json_rest_courses & .import_courses via TDD & refactor

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,18 @@
-class Course < ApplicationRecord
+require 'open-uri' # we need to requiree it in order to avoid conflicts with Kernel#open  *see line 17
+
+class Course
+  include ActiveModel::Model
+
+  attr_accessor :title, :url, :slug, :author, :next_start, :next_start_formatted, :price, :start_dates
+
+  def self.import_courses
+    json_rest_courses['courses'].map do |key, values|
+      Course.new(values.merge(slug: key))
+    end
+  end
+
+  private_class_method def self.json_rest_courses
+    url = 'https://careerfoundry.com/en/api/courses/'
+    JSON.parse(open(url).read) # rubocop:disable Security/Open
+  end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:raw_courses_json) { JSON.parse(file_fixture('courses.json').read) }
+
+  describe '.import_courses' do
+    let(:courses_title) { Course.import_courses.map(&:title) }
+
+    it 'add courses from the api' do
+      expect(Course).to receive(:json_rest_courses).and_return(raw_courses_json) # rubocop:disable RSpec/MessageSpies
+      expect(courses_title).to include('Become a Web Developer')
+    end
+  end
 end
